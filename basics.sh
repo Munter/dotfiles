227eb2bc05ccb875f39e7afe4d47266096dd734d
Peter Müller <munter@fumle.dk>
2013-05-12 23:29:26 +0200

    Build node v0.8.23 in basics.sh

--- basics.sh
+++ basics.sh
@@ -1,8 +1,8 @@
 sudo apt-get install git chromium-browser pngquant optipng pngcrush libssl-dev libcairo-dev libjpeg-dev libgif-dev build-essential tree htop
 
-git clone https://github.com/joyent/node.git
+git clone git://github.com/joyent/node.git
 cd node
-git checkout v0.6.20
+git checkout v0.8.23-release
 ./configure --prefix=~/.local
 make
 make install
