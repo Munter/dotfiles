d3edb77241eedbc28a04283bbfd756d92ee9f7c5
Peter Müller <munter@fumle.dk>
2013-05-15 23:41:13 +0200

    basics.sh: Switched to nvm for node install

--- basics.sh
+++ basics.sh
@@ -1,12 +1,7 @@
-sudo apt-get install git chromium-browser pngquant optipng pngcrush libssl-dev libcairo-dev libjpeg-dev libgif-dev build-essential tree htop
+sudo apt-get install git screen chromium-browser pngquant optipng pngcrush libssl-dev libcairo-dev libjpeg-dev libgif-dev build-essential tree htop
 
-git clone git://github.com/joyent/node.git
-cd node
-git checkout v0.8.23-release
-./configure --prefix=~/.local
-make
-make install
-cd ..
-rm -rf node
-
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+~/.nvm/nvm.sh
+nvm install v0.8
+nvm alias default v0.8
 
